82dd25e00bb34ac3f8ca5d631f714b901b3ec6cf
Samuel Wallace <samueltwallace@gmail.com>
2020-10-04 23:53:30 -0500

    accesses more files now

--- .config/rofi/pdf.sh
+++ .config/rofi/pdf.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
 PATH="$HOME/.local/bin:$PATH"
 
-PDFS="$(find $HOME/Documents -name *.pdf)"
-PDFS="$(find $HOME/repos -name *.pdf)\n$PDFS"
-PDF="$(echo "$PDFS" | rofi -p Zathura -dmenu -i)"
+PDF="$(find $HOME/**/*.pdf | rofi -p Zathura -dmenu -i)"
 if [[ -f "$PDF" ]] 
 then
 	zathura "$PDF"
